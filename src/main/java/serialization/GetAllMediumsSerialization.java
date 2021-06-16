d89e2666b8b6182c20381fd4c37fd1290989dad7
swyze <alex99.martin@orange.fr>
2021-06-16 10:07:23 +0200

    GetMedium & GetAllMediums

---
+++ src/main/java/serialization/GetAllMediumsSerialization.java
@@ -0,0 +1,51 @@
+package serialization;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.mycompany.spiritus.metier.model.Astrologue;
+import com.mycompany.spiritus.metier.model.Cartomancien;
+import com.mycompany.spiritus.metier.model.Medium;
+import com.mycompany.spiritus.metier.model.Spirite;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class GetAllMediumsSerialization extends Serialization {
+
+    public void serialize(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        List<Medium> mediumList = (List<Medium>) request.getAttribute("mediumList");
+
+        JsonObject container = new JsonObject();
+
+        if (mediumList != null) {
+            for (Medium medium : mediumList) {
+
+                JsonObject mediumJson = new JsonObject();
+                mediumJson.addProperty("denomination", medium.getDenomination());
+                mediumJson.addProperty("gender", medium.getGender().toString());
+                mediumJson.addProperty("gender", medium.getPresentation());
+                mediumJson.addProperty("mediumType", medium.getClass().getName());
+
+                if (medium instanceof Spirite) {
+                    mediumJson.addProperty("support", ((Spirite) medium).getSupport());
+                } else if (medium instanceof Astrologue) {
+                    mediumJson.addProperty("formation", ((Astrologue) medium).getFormation());
+                    mediumJson.addProperty("promotion", ((Astrologue) medium).getPromotion());
+                }
+                container.add(medium.getId().toString(), mediumJson);
+            }
+        }
+
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+        gson.toJson(container, out);
+        out.close();
+    }
+
+}
