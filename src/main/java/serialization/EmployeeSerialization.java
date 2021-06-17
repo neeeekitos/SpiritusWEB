1e2b5492097afc47c53d28b9d707531ee26e20f4
galiixy <gaelle_f@live.fr>
2021-06-17 16:57:40 +0200

    Employee Home - JS + HTML

--- src/main/java/serialization/EmployeeSerialization.java
+++ src/main/java/serialization/EmployeeSerialization.java
@@ -106,6 +106,7 @@ package serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.mycompany.spiritus.metier.model.Consultation;
 import com.mycompany.spiritus.metier.model.Employee;
@@ -118,6 +119,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.List;
 import java.util.TreeMap;
@@ -151,33 +153,42 @@ public class EmployeeSerialization extends Serialization {
             if (consultation != null) {
                 pendingConsult.addProperty("status", consultation.getStatus().toString());
                 pendingConsult.addProperty("date", consultation.getDate().toString());
+                pendingConsult.addProperty("medium_ID", consultation.getMedium().getId());
+                pendingConsult.addProperty("medium_nom", consultation.getMedium().getDenomination());
+                pendingConsult.addProperty("client_ID", consultation.getClient().getId());
+                pendingConsult.addProperty("client_nom", consultation.getClient().getFirstName() + consultation.getClient().getLastName());
                 container.add("pendingConsultation", pendingConsult);
             }
 
             JsonObject histoConsult = new JsonObject(); // Objet histoConsult
             List<Consultation> consultations = (List<Consultation>) request.getAttribute("historiqueConsultation"); // recuperation de l'attribut
+            JsonArray HistoriqueArray = new JsonArray();
             if (consultations != null) {
                 for (Consultation c : consultations) {
-                    JsonObject consult = new JsonObject(); // Objet consult
-                    consult.addProperty("status", c.getStatus().toString());
-                    consult.addProperty("date", c.getDate().toString());
-                    consult.addProperty("comment", c.getComment());
-                    consult.addProperty("medium_ID", c.getMedium().getId());
-                    consult.addProperty("medium_nom", c.getMedium().getDenomination());
-                    consult.addProperty("client_ID", c.getClient().getId());
-                    consult.addProperty("client_nom", c.getClient().getFirstName() + c.getClient().getLastName());
-                    histoConsult.add(c.getId().toString(), consult);
+                    String formattedDate = "";
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    formattedDate = sdf.format(c.getDate());
+                    JsonObject consultationObject = new JsonObject();
+                    consultationObject.addProperty("status", c.getStatus().toString());
+                    consultationObject.addProperty("date", c.getDate().toString());
+                    consultationObject.addProperty("comment", c.getComment());
+                    consultationObject.addProperty("medium_ID", c.getMedium().getId());
+                    consultationObject.addProperty("medium_nom", c.getMedium().getDenomination());
+                    consultationObject.addProperty("client_ID", c.getClient().getId());
+                    consultationObject.addProperty("client_nom", c.getClient().getFirstName() + c.getClient().getLastName());
+                    HistoriqueArray.add(consultationObject);
                 }
-                container.add("Historique", histoConsult);
+                container.add("Historique", HistoriqueArray);
             }
-            
+
             HashMap<Employee, Long> employeeList = (HashMap<Employee, Long>) request.getAttribute("topEmployee"); // recuperation de l'attribut
             //tri HashMap
             ValueComparator comparateur = new ValueComparator(employeeList);
             TreeMap<Employee, Long> mapTriee = new TreeMap<Employee, Long>(comparateur);
             mapTriee.putAll(employeeList);
 
-            JsonObject topEmployee = new JsonObject(); // Objet liste topEmployé
+            JsonArray topEmployee = new JsonArray();
+
             Integer limite = mapTriee.size();
             if (limite >= 5) {
                 limite = 5;
@@ -188,7 +199,7 @@ public class EmployeeSerialization extends Serialization {
                 Employee objEmployee = (Employee) mapTriee.keySet().toArray()[i];
                 employee.addProperty("nom", objEmployee.getFirstName() + objEmployee.getLastName());
                 employee.addProperty("nombreConsult", employeeList.get(objEmployee).toString());
-                topEmployee.add(i.toString(), employee);
+                topEmployee.add(employee);
             }
             container.add("topEmployee", topEmployee);
 
--- src/main/webapp/js/employee_dashboard.js
+++ src/main/webapp/js/employee_dashboard.js
@@ -1,37 +1,14 @@
-<<<<<<< HEAD
 (function(namespace, $, undefined) {
 
     namespace.hello = function () {
         alert("hello world");
     }
-
-    $(document).ready( function () {
-        $.ajax({
-         url: 'ActionServlet',
-         method: 'POST',
-         data:{
-             todo:'getEmployeeHomePageInfosAction'
-         },
-         dataType:'json'
-     })
-      .done(function(response){
-         console.log('response', response);
-     })
-     .fail( function (error) { // Appel KO => erreur technique à gérer
-         console.log('Erreur:', error); // LOG sur la Console Javascript
-         alert('Erreur lors du chargement des données: HTTP Code ' + error.status); // Popup d'erreur
-     });
-        
-    });
-
-}) (window.SpiritusAccountCreation = window.SpiritusAccountCreation || {}, jQuery);
-=======
-(function(namespace, $, undefined) {
-
-    namespace.hello = function () {
-        alert("hello world");
+    
+    function showMediumModal() {
+        alert("Medium info");
+        $("#modal").css("display", "block");
     }
-
+    
     $(document).ready( function () {
         $.ajax({
          url: 'ActionServlet',
@@ -44,13 +21,75 @@
       .done(function(data){
         console.log('response', data);
         const available = data.Employee.available;
-        if (available){
-            $(".available").text("Disponibilité : disponible");
-        }else{
-            $(".available").text("Disponibilité : indisponible");
+        const pendingConsult = data.pendingConsultation;
 
+        var data_tab;
+        var line = $("<tr></tr>");
+        if (available){
+            $(".available").text("Vous êtes prêt à recevoir une consultation !");
+            $("#available").attr("src", "./images/available.png");
+            data_tab = $("<td colspan='4'><center>Aucune consultation en cours</center></td>");
+        }else{
+            $(".available").text("Vous avez une demande à traiter. ");
+            $("#available").attr("src", "./images/unavailable.png");
+            
+            data_tab = $("<td></td>");
+            data_tab.text(pendingConsult.date);
+            line.append(data_tab);
+
+            data_tab = $("<td></td>");
+            data_tab.text(pendingConsult.medium_nom);
+            line.append(data_tab);
+
+            data_tab = $("<td></td>");
+            data_tab.text(pendingConsult.client_nom);
+            line.append(data_tab);
+            
+            data_tab = $("<td></td>");
+            data_tab.text(pendingConsult.status);
+            line.append(data_tab);
+        }
+        line.append(data_tab);
+        $("#pendingConsult tbody").append(line);
+
+        const historiqueConsult = data.Historique;
+        if (historiqueConsult ==  null){
+            data_tab = $("<td colspan='5'><center>Aucune consultation en historique</center></td>");
+            line.append(data_tab);
+            $("#historiqueConsult tbody").append(line);
+        }else{
+           historiqueConsult.forEach(cons => {
+                line = $("<tr></tr>");
+                
+                data_tab = $("<td></td>");
+                data_tab.text(cons.date);
+                line.append(data_tab);
+                
+                data_tab = $("<td></td>");
+                data_tab.text(cons.medium_nom);
+                data_tab.attr("id", cons.medium_ID);
+                data_tab.click(showMediumModal);
+                line.append(data_tab);
+                
+                data_tab = $("<td></td>");
+                data_tab.text(cons.client_nom);
+                data_tab.attr("id", cons.client_ID);
+                line.append(data_tab);
+
+                data_tab = $("<td></td>");
+                data_tab.text(cons.status);
+                line.append(data_tab);
+
+                data_tab = $("<td></td>");
+                data_tab.text(cons.comment);
+                line.append(data_tab);
+                
+                $("#historiqueConsult tbody").append(line);
+            })  
         }
         
+       
+     
      })
      .fail( function (error) { // Appel KO => erreur technique à gérer
          console.log('Erreur:', error); // LOG sur la Console Javascript
@@ -60,4 +99,3 @@
     });
 
 }) (window.EmployeeHomePage = window.EmployeeHomePage || {}, jQuery);
->>>>>>> a7e4c19 (Employee Home - Html CSS)
